140c326e9cd83c187c876577e521bfffd82a5b0f
Dr. David A. Kunz <david.kunz@sap.com>
2025-08-12 15:30:55 +0200

    chore: refactor

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 #!/usr/bin/env node
 
-import run from './lib/index.js'
+import run, { runTool } from './lib/index.js'
 
 const args = process.argv.slice(2)
-const options = {}
 
 if (args.length > 0 && !args[0].startsWith('-')) {
   const toolName = args[0]
   const toolArgs = args.slice(1)
-  options.tools = toolName
-  options.args = toolArgs
+  runTool(toolName, ...toolArgs)
+} else {
+  run()
 }
-
-run(options)
